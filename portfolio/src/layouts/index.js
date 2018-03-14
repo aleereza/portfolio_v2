b4d3a21b35df1c60f535750f63986ce746b7b24b
aleerezaa <alireza.barkhordari@gmail.com>
2018-03-13 19:58:41 -0700

    trying to add live dimension

--- portfolio/src/layouts/index.js
+++ portfolio/src/layouts/index.js
@@ -2,29 +2,21 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
-import Header from '../components/Header'
+import MainContent from '../components/MainContent'
 import './index.css'
 
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet
-      title="Gatsby Default Starter"
+      title="Alireza's portfolio"
       meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        { name: 'description', content: 'portfolio' },
+        { name: 'keywords', content: 'web developer, Reactjs, Django' },
       ]}
     />
-    <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
+    <MainContent>
       {children()}
-    </div>
+    </MainContent>
   </div>
 )
 
